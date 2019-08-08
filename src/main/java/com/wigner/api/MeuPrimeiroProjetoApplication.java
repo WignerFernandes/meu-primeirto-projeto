48cbe180f01114773101e511c8c64bdb2ec0d59c
Wigner Fernandes <wigner.fernandes@gmail.com>
2019-08-08 16:52:40 -0300

    modificado até a aula 16

--- src/main/java/com/wigner/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/wigner/api/MeuPrimeiroProjetoApplication.java
@@ -1,14 +1,35 @@
 package com.wigner.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wigner.api.utis.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
+	/*
+	 * @Value ( "${paginacao.qtd_por_pagina}" ) private int qtdPorPagina ;
+	 */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("Meu Primeiro Projeto!");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encode:" + senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha endcoded novamente:" + senhaEncoded);
+			
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
+	}
+	
 }
